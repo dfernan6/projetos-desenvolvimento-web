d5a63f2b2262d921bc56a0a1677a1b1ae3db54ce
dfernan6 <danilo.afernandes@outlook.com>
2023-07-02 08:14:28 -0300

    Iniciando projetos fundamentos NextJS

---
+++ fundamentos/src/pages/_app.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return (
+           <div>
+             <h1>Hello World!</h1>
+           </div>
+          )
+}
---
+++ fundamentos/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import App from './_app'
+
+export default function Home() {
+  return (
+    <div>
+      {App}
+    </div>
+  )
+}
